3ba7a119a69187f14aa48a8a93f353faa11341ae
ThinkRedstone <ittai11@gmail.com>
2017-08-25 19:23:07 +0300

    split args.basicAuthCreds into user and password

--- server_modules/args.js
+++ server_modules/args.js
@@ -1,6 +1,7 @@
 var argv = require('minimist')(process.argv.slice(2));
 
 exports.port = argv.p || 1390;
-exports.basicAuthCreds = argv.u;
+exports.user = argv.u ? argv.u.split(":")[0] : "";
+exports.password = argv.u ? argv.u.split(":")[1] : "";
 exports.datadir = argv.d || 'data';
 exports.slaveMode = argv.s || false;
